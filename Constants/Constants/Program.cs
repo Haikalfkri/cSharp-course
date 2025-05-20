dd7d80ab38da0b25f4d3cfd15a7f443ce6fdeca0
Haikal Fikri <haikaljr1245@gmail.com>
2025-05-21 06:16:56 +0700

    constants

---
+++ Constants/Constants/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Constant Section");
+
+            // Constants = immutable values which are known at compile time and do not change for the life
+            //             of the program. Constants are declared using the const keyword.
+
+            const double PI = 3.14; // constant declaration
+
+            Console.WriteLine(PI);
+            Console.ReadKey();
+        }
+    }
+}
