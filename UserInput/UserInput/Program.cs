f79c7e77a03ce8f101c1e56e4eb2e871c3158c3d
Haikal Fikri <haikaljr1245@gmail.com>
2025-05-21 06:41:43 +0700

    user input

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // user input section
+
+            Console.Write("Enter Your Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Your Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your name is " + name);
+            Console.WriteLine("You age is " + age);
+
+            Console.ReadKey();
+        }
+    }
+}
