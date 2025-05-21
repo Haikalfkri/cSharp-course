7f759fca9666a9cbe8e2f23a9b0ba13226fb80c2
Haikal Fikri <haikaljr1245@gmail.com>
2025-05-22 04:58:29 +0700

    arithmatic operators

---
+++ ArithmaticOperators/ArithmaticOperators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ArithmaticOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Artihmatic Operators
+            // +, -, *, /, %
+
+            int friends = 4;
+
+            //friends = friends + 1;
+            //friends += 1;
+            //friends++;
+
+            //friends = friends - 1;
+            //friends -= 1;
+            //friends--;
+
+            //friends = friends * 2;    
+            //friends *= 2;
+
+            //friends = friends / 2;
+            //friends /= 2;
+
+
+
+            Console.WriteLine(friends);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
