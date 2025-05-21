727d6f73baf1967c3e16dbd18d1a67ef7af1b51b
Haikal Fikri <haikaljr1245@gmail.com>
2025-05-22 05:34:39 +0700

    hypotenuse calculator

---
+++ HypotenuseCalculatorProgram/HypotenuseCalculatorProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HypotenuseCalculatorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hypotenuse Calculator!");
+
+            Console.Write("Enter Side A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Side B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt((a * a) + (b * b));
+
+            Console.WriteLine("The Hypotenuse is: " + c);
+
+            Console.ReadKey();
+
+        }
+    }
+}
