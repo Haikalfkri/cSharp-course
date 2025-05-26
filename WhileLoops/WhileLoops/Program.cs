296957d07139bd07d057b42d92b4219b05836172
Haikal Fikri <haikaljr1245@gmail.com>
2025-05-27 04:15:59 +0700

    While-Loops

---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while loops
+
+            string name = "";
+            string exit = "";
+
+            while (exit != "exit")
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+
+                Console.Write("Again? ");
+                exit = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hello " + name);
+
+            Console.ReadKey();
+        }
+    }
+}
