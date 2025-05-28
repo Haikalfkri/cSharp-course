2b9f928a59ecfae3e507682b155b5d15f0323b84
Haikal Fikri <haikaljr1245@gmail.com>
2025-05-29 05:13:02 +0700

    Foreach

---
+++ Foreach/Foreach/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // foreach loop = a simpler way to iterate over an array, but it's less flexible
+
+            string[] cars = { "BMW", "Lambo", "Mustang" };
+
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
